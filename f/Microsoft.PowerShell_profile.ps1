453586fa8f26b77216bb8614c367359ea799badf
Partha P. Das <parthopdas@live.com>
2021-06-13 09:45:24 +0200

    Changes
    > New settings
    > Cleaning up install script
    > Installer for scoop stuff

--- f/Microsoft.PowerShell_profile.ps1
+++ f/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,20 @@
-echo "Hello!"
+#requires -Modules PSReadLine, posh-git
+
+if ($host.Name -ne 'ConsoleHost') {
+  # TODO: Implement stuff we want for all hosts.
+  return
+}
+
+$modules = @('PSReadLine', 'posh-git')
+
+$modules | ForEach-Object {
+  if (-not (Get-Module -ListAvailable -Name $_)) {
+    Write-Host -ForegroundColor White "Installing module $_..."
+    Install-Module $_ -Force -Scope CurrentUser
+  }
+
+  Write-Host -ForegroundColor White "Loading module $_..."
+  Import-Module $_
+}
+
+Set-Location d:\
--- install.ps1
+++ install.ps1
@@ -1,62 +1,48 @@
+#requires -RunAsAdministrator
+#requires -PSEdition Core
+#requires -Version 7.1.3
+
 <#
-TODO: autohotkey at startup
 TODO: make it work for linux
+TODO: symlinks to c:\users\partho,parthapdas, vs community vs enterprise
+TODO: git-credential-manager-core
 #>
 
+$filesRoot = Join-Path $PSScriptRoot "f"
 $files = @(
+  #
+  # Common
+  #
   @{
-    "name" = "PowerShell Profile"
+    "name" = "PowerShell Core Profile"
     "target" = Join-Path $env:USERPROFILE "Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
     "platform" = "all"
+  },
+  @{
+    "name" = "Git settings"
+    "target" = Join-Path $env:USERPROFILE ".gitconfig"
+    "platform" = "all"
+  }
+
+  #
+  # Windows only
+  #
+  @{
+    "name" = "PowerShell Profile"
+    "target" = Join-Path $env:USERPROFILE "Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1"
+    "platform" = "win"
   },
   @{
     "name" = "Microsoft Terminal settings"
     "target" = Join-Path $env:LOCALAPPDATA "Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
     "platform" = "win"
   }
+  @{
+    "name" = "Git Extensions settings"
+    "target" = Join-Path $env:USERPROFILE "scoop/apps/gitextensions/current/GitExtensions.settings"
+    "platform" = "win"
+  }
 )
-
-function Test-IsPowerShellCore {
-  [CmdletBinding(ConfirmImpact = 'None')]
-  [OutputType([bool])]
-  param ()
-
-  process
-  {
-    $PSVersionTable.PSEdition -ceq "Core"
-  }
-}
-
-# NOTE: From https://gist.github.com/jhochwald/46014a3de425dc21c1f1f7e31cd49cf1
-function Test-IsAdmin {
-  [CmdletBinding(ConfirmImpact = 'None')]
-  [OutputType([bool])]
-  param ()
-
-  process
-  {
-    if ($PSVersionTable.PSEdition -eq 'Desktop') {
-      # Fastest way on Windows
-      ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
-    } elseif (($PSVersionTable.PSEdition -eq 'Core') -and ($PSVersionTable.Platform -eq 'Unix')) {
-      # Ok, on macOS and Linux we use ID to figure out if we run elevated (0 means superuser rights)
-      if ((id -u) -eq 0) {
-        $true
-      } else {
-        $false
-      }
-    } elseif (($PSVersionTable.PSEdition -eq 'Core') -and ($PSVersionTable.Platform -eq 'Win32NT')) {
-      # For PowerShell Core on Windows the same approach as with the Desktop work just fine
-      # This is for future improvements :-)
-      ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
-    } else {
-      # Unable to figure it out!
-      Write-Warning -Message 'Unknown'
-
-      $false
-    }
-  }
-}
 
 function Test-ReparsePoint {
   param (
@@ -97,7 +83,7 @@ function Install-SettingsFile {
   Process {
     Write-Host -ForegroundColor Green "Installing $($SettingsInfo.name)..."
 
-    $source = Join-Path $PSScriptRoot "f" | Join-Path -ChildPath (Split-Path $SettingsInfo.target -Leaf)
+    $source = $filesRoot | Join-Path -ChildPath (Split-Path $SettingsInfo.target -Leaf)
     Write-Host -ForegroundColor Gray "... Checking local file exists. " -NoNewline
     if (-not (Test-Path $source)) {
       Write-Host -ForegroundColor Red "$source does not exist. Aborting installation of this setting!"
@@ -144,14 +130,11 @@ function Install-SettingsFile {
   }
 }
 
-if (-not (Test-IsPowerShellCore)) {
-  Write-Host -ForegroundColor Red "Script requires PowerShell Core. Quitting!"
-  return
-}
-
-if (-not (Test-IsAdmin)) {
-  Write-Host -ForegroundColor Red "Script requires elevated privileges. Quitting!"
-  return
-}
-
 $files | Install-SettingsFile
+
+if ($IsWindows) {
+  Write-Host -ForegroundColor Gray "Installing windows startup scripts... " -NoNewline
+  $autoExecBat = $filesRoot | Join-Path -ChildPath "autoexec.bat"
+  New-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "AutoHotKey" -Value "cmd /c $autoExecBat" -PropertyType "String" -Force | Out-Null
+  Write-Host -ForegroundColor Green "done!"
+}
